fa0fd29191c8192469f6e387a1990d324e604565
Lokesh <lokeshz9999@gmail.com>
2026-01-12 18:32:10 +0530

    constant,config,data_ingestion completed

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,57 @@
+from src.constants import *
+from src.config.configuration import *
+import os, sys
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+from src.logger import logging
+from src.exception import CustomException
+
+#Difining configuartion
+@dataclass
+class DataIngestionConfig:
+    train_data_path:str = TRAIN_FILE_PATH
+    test_data_path:str = TEST_FILE_PATH
+    raw_data_path:str = RAW_FILE_PATH
+
+#in this class data extract and split will happen
+class DataIngestion:
+    def __init__(self):#defining constructor
+        self.data_ingestion_config = DataIngestionConfig()#creating data_ingestion_confif to call class DataIngestionConfig
+    
+    def initiate_data_ingestion(self):
+        try:
+            df= pd.read_csv(DATASET_PATH)#reading dataset from DATASET_PATH
+            #creating raw data folder
+            os.makedirs(os.path.dirname(self.data_ingestion_config.raw_data_path),exist_ok=True)
+
+            #saving raw data in csv format in raw data folder
+            df.to_csv(self.data_ingestion_config.raw_data_path,index=False)
+
+            #splitting data into train and test 
+            train_set, test_set = train_test_split(df, test_size=0.20, random_state=42)
+
+            #creating folder to store splitted train data
+            os.makedirs(os.path.dirname(self.data_ingestion_config.train_data_path),exist_ok=True)
+            #saving train data in csv format
+            train_set.to_csv(self.data_ingestion_config.train_data_path,header=True,)
+
+            #creating folder to store splitted test data
+            os.makedirs(os.path.dirname(self.data_ingestion_config.test_data_path),exist_ok=True)
+            #saving test data in csv format
+            test_set.to_csv(self.data_ingestion_config.test_data_path,header=True,)
+
+            return(
+                self.data_ingestion_config.train_data_path,
+                self.data_ingestion_config.test_data_path,
+            )
+
+            
+        except Exception as e:
+            raise CustomException(e, sys)
+
+
+if __name__ == "__main__":
+    obj = DataIngestion()
+    train_data, test_data = obj.initiate_data_ingestion()
---
+++ src/constants/__init__.py
@@ -0,0 +1,35 @@
+import os, sys
+from datetime import datetime
+
+#to create arifact folder this is directory in which pipeline output like data ingestion, 
+# data transformation, model training is stored
+#artifact folder-->pipeline folder-->output with timestamp
+def get_current_time_stamp():#to get current timestamp
+    return f"{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}"
+
+CURRENT_TIME_STAMP = get_current_time_stamp()
+
+#defining dataset
+ROUTE_DIR_KEY = os.getcwd()     #getcwd is used to find current working directory
+DATA_DIR= "dataset"
+DATA_DIR_KEY= "finalTrain.csv"
+
+#Create articat directory/artifact folder to store output of pipeline
+ARTIFACT_DIR_KEY = "Artifact"
+
+#Difining data ingestion related variable: 
+DATA_INGESTION_KEY = "data ingestion"    #CREATED THIS FOLDER IN ARTIFACT
+#creating two folders named DATA_INGESTION_RAW_DATA and DATA_INGESTION_INGESTED_DATA_DIR_KEY 
+# in artifact folder
+
+DATA_INGESTION_RAW_DATA_DIR = "raw_data_dir"  
+#STORING RAW.CSV DOWNLOADED DATA IN THIS FOLDER
+
+DATA_INGESTION_INGESTED_DATA_DIR_KEY = "ingested_dir"   
+#STORING TRAIN (train.csv) AND TEST (test.csv) in their special created folder in ingested_dir
+
+#RAW_DATA_DIR_KEY will be created in DATA_INGESTION_RAW_DATA folder
+RAW_DATA_DIR_KEY = "raw.csv"
+
+TRAIN_DATA_DIR_KEY = "train.csv"
+TEST_DATA_DIR_KEY = "test.csv"
